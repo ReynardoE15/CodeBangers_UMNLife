2d24fccb793937c83fa339eb154912c8d9c448ac
auddl <10horysheet10@gmail.com>
2023-03-18 15:04:21 +0700

    test push dari auddly

--- app/src/main/java/id/ac/umn/codebangers_umnlife/model/Mahasiswa.kt
+++ app/src/main/java/id/ac/umn/codebangers_umnlife/model/Mahasiswa.kt
@@ -5,6 +5,7 @@ class Mahasiswa(val nama:String, var kenyang: Int = 50,
                 var motivasi: Int = 50, var pengetahuan: Int = 0)
 {
 
+    //sdsd
     fun tambahiKenyang(angka: Int) {
         kenyang += angka
     }
